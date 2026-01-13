37bdf441e462efe3fef97d892915d07da43c4538
igurupreeth <igurupreeth@gmail.com>
2026-01-13 12:12:56 +0530

    java project for anjan and bharath, started today literals chapter

---
+++ Core_java_anjan_bharath/src/java_basics/Literals.java
@@ -0,0 +1,28 @@
+package java_basics;
+
+// Literals : types of variables, 4 types literals. 
+// Number literals - int, decimals( float, double), without any quotes
+// character literals - single quotes   'a' 'b' '2'  '$' 
+// string literals - more than one character is considered as string, double quotes. 
+// boolean literals - true = 1, false = 0, ( without any quotes)
+
+public class Literals 
+{
+   public static void main(String [] args)
+   {
+	   System.out.println(5);
+	   System.out.println(2.3);
+	   System.out.println('a');
+	   System.out.println('5');  // character not number
+	   System.out.println('@');  // character not a symbol 
+	   
+	   System.out.println("Bharath");  // string
+	   System.out.println("Anjan");    // string
+	   System.out.println("1234");  // string 
+	   System.out.println("abc123 #$%^%");
+	   
+	   System.out.println(true);
+	   System.out.println(false);
+	   
+   }
+}
