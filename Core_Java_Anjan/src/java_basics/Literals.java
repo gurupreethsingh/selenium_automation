2965690a960fe9b56765d298c1b3a85d8eaf37e1
anjan121 <anjansa35@gmail.com>
2026-01-14 14:49:31 +0530

    I have started my automation testing project

---
+++ Core_Java_Anjan/src/java_basics/Literals.java
@@ -0,0 +1,22 @@
+package java_basics;
+
+public class Literals {
+
+	public static void main(String[] args) {
+
+		   System.out.println(5);
+		   System.out.println(2.3);
+		   System.out.println('a');
+		   System.out.println('5');  
+		   System.out.println('@');   
+		   
+		   System.out.println("Bharath");  
+		   System.out.println("Anjan");    
+		   System.out.println("1234");  
+		   System.out.println("abc123 #$%^%");
+		   
+		   System.out.println(true);
+		   System.out.println(false);
+	}
+
+}
