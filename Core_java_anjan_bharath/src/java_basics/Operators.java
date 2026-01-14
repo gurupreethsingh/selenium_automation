cb62cf8c0320f62ca927aaf1b1549341c493a51e
igurupreeth <igurupreeth@gmail.com>
2026-01-14 13:08:30 +0530

    varaibles and operatores in java class

---
+++ Core_java_anjan_bharath/src/java_basics/Operators.java
@@ -0,0 +1,30 @@
+package java_basics;
+
+public class Operators {
+
+	public static void main(String[] args) 
+	{
+		// Arithematic operators ( + , -, * , /,  %)
+		int a = 7; 
+		int b = 2; 
+		float a1 = 7f;
+		float b1 = 2f; 
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);   // 3.5 it will give  only 3, 
+		System.out.println(a1/b1);   // 3.5
+		System.out.println(a%b);
+		
+		// relational operators   <  >  <=  >=  == !=  ( result in boolean) 
+		System.out.println(a < b);
+		System.out.println(a <= b);
+		System.out.println(a == b);
+		System.out.println(a != b);
+		System.out.println(a > b);
+		System.out.println(a >= b);
+		
+		// logical operaters (and , or, not) 
+		// logical and operator. 
+	}
+}
---
+++ Core_java_anjan_bharath/src/java_basics/Variables.java
@@ -0,0 +1,29 @@
+package java_basics;
+
+public class Variables     // jvm - look for main function to start the program.
+{
+    public static void main(String[] args) 
+    {
+		// variables : are named memory locations. it can store any value, during execution we can 
+    	// change that value 
+    	
+       int a = 5;
+       long l = 8765875698769876l;   // long integer  8 bytes
+       float f = 2.3f;    // 4 bytes
+       double d = 3.4;    // 8 bytes
+       char ch = 'a';
+       boolean result = true; 
+       boolean result1 = false; 
+       
+       
+       System.out.println(a);
+       System.out.println(l);
+       System.out.println(f);
+       System.out.println(d);
+       System.out.println(ch);
+       System.out.println(result);
+       System.out.println(result1);
+       
+       
+	}
+}
