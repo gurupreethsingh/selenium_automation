99b797d9a7b56479911ad33ca552a28ef34223a5
igurupreeth <igurupreeth@gmail.com>
2026-02-19 12:13:58 +0530

    all verification class updated with text is present. elemetn is clickable., and alert is handled functions, add to wishlist and remove from wishlist updated.

--- ecommerse_automation_practice/src/generic/AllVerifications.java
+++ ecommerse_automation_practice/src/generic/AllVerifications.java
@@ -2,6 +2,7 @@ package generic;
 
 import java.time.Duration;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -51,6 +52,11 @@ public class AllVerifications
 		{
 			wait.until(ExpectedConditions.textToBePresentInElement(element, expectedText));
 			textVerified = true;
+			if(textVerified == true)
+			{
+				System.out.println("Text is matching " + expectedText + " Found : "+ element.getText());
+			}
+			
 		}
 		catch(Exception ex)
 		{
@@ -58,4 +64,45 @@ public class AllVerifications
 		}
 		return textVerified;
 	}
+	
+	
+	public static boolean clickOnElementIfVisibleAndEnabled(WebDriver driver, WebElement element)
+	{
+		boolean elementClicked = false; 
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		try
+		{
+			wait.until(ExpectedConditions.elementToBeClickable(element));
+			element.click();
+			elementClicked = true; 
+			System.out.println("Element clicked successfully : " + element);
+			
+		}
+		catch(Exception ex)
+		{
+			System.out.println("Element is not clickable or Visible : "+ element);
+			ex.printStackTrace();
+		}
+		return elementClicked;
+	}
+	
+	public static boolean handleAlertIfAlertIsPresentAndAccept(WebDriver driver)
+	{
+		boolean alertHandled = false; 
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		try
+		{
+			wait.until(ExpectedConditions.alertIsPresent());
+			Alert alert = driver.switchTo().alert();
+			alert.accept();
+			System.out.println("Alert is handeled successfully, navigating to next page.");
+		}
+		catch(Exception ex)
+		{
+			System.out.println("Unable to handle the alert.");
+			ex.printStackTrace();
+		}
+		return alertHandled;
+	}
+
 }
--- ecommerse_automation_practice/src/scripts/shoppage_system_scenarios/AS_9054_RemoveProductFromWishlistPageAfterLogin.java
+++ ecommerse_automation_practice/src/scripts/shoppage_system_scenarios/AS_9054_RemoveProductFromWishlistPageAfterLogin.java
@@ -8,10 +8,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import generic.AllVerifications;
 import generic.AutomationConstants;
 
-public class AS_9054_AddProductToWishlistFromShopAllPageAfterLogin implements AutomationConstants
+public class AS_9054_RemoveProductFromWishlistPageAfterLogin implements AutomationConstants
 {
 
 	public static void main(String[] args) throws InterruptedException
@@ -27,7 +26,12 @@ public class AS_9054_AddProductToWishlistFromShopAllPageAfterLogin implements Au
 		
 		// step 2 : click sign in button and navigate to login page . 
 		WebElement signinButton = driver.findElement(By.xpath("//span[@class='text-[12px] font-extrabold text-white']"));
-		AllVerifications.clickOnElementIfVisibleAndEnabled(driver, signinButton);
+		Thread.sleep(500);
+		// click on the logo
+		signinButton.click();
+		Thread.sleep(500);
+		System.out.println(driver.getTitle());
+		
 		
 		WebElement emailField = driver.findElement(By.xpath("//input[@id='email']"));
 		Thread.sleep(500);
@@ -40,42 +44,72 @@ public class AS_9054_AddProductToWishlistFromShopAllPageAfterLogin implements Au
 		Thread.sleep(500);
 		
 		WebElement loginButton = driver.findElement(By.xpath("//button[@class='w-full py-2 px-4 bg-gradient-to-r from-orange-500 to-amber-400 text-white font-bold rounded-full shadow hover:from-orange-500 hover:to-orange-300 transition']"));
-		AllVerifications.clickOnElementIfVisibleAndEnabled(driver, loginButton);
+		Thread.sleep(500);
+		loginButton.click();
+		Thread.sleep(500);
 		
 		// handle the alert by switching to alert window and click on ok button on the alert
-		AllVerifications.handleAlertIfAlertIsPresentAndAccept(driver);
+		Alert alert = driver.switchTo().alert();
+		System.out.println(alert.getText());
+		alert.accept(); // dissmiss()
+		Thread.sleep(500);
+		System.out.println(driver.getTitle());
 		
 		// navigate to shop page by clicking on shop all link from the header of dashboard page. 
 		WebElement shopAllLink = driver.findElement(By.xpath("//a[@class='linkText text-sm transition-colors text-gray-700 hover:text-orange-600']"));
-		AllVerifications.clickOnElementIfVisibleAndEnabled(driver, shopAllLink);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		shopAllLink.click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		System.out.println(driver.getTitle());
 		
-		// click on the wishlist icon of any one product 
+		// click on the wishlist icon of the product you want to remove from wishlist
 		WebElement wishlistIconOnProduct = driver.findElement(By.xpath("(//button[@class='absolute top-2 right-2 h-9 w-9 rounded-full bg-white/90 backdrop-blur flex items-center justify-center transition hover:bg-white'])[1]"));
-		AllVerifications.clickOnElementIfVisibleAndEnabled(driver, wishlistIconOnProduct);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		wishlistIconOnProduct.click();
+		Thread.sleep(500);
 		
-		// find the wishlisticon from header and click on it and navigate to wishlist page. 
+		// find the wishlist icon from header and click on it and navigate to wishlist page. 
 		
 		WebElement wishlistIconHeader = driver.findElement(By.xpath("(//a[@href='/wishlist'])[1]"));
-		AllVerifications.clickOnElementIfVisibleAndEnabled(driver,  wishlistIconHeader);
+		Thread.sleep(500);
+		wishlistIconHeader.click();
+		Thread.sleep(500);
+		System.out.println(driver.getTitle());
 		
 		// verify the product is added to wishlist or not 
-		String expectedProductText = "Yoga Resistance Bands Set";
+//		String expectedProductText = "Yoga Resistance Bands Set";
 		
-		WebElement productNameWishlistpage = driver.findElement(By.xpath("//h2[@class='text-[13px] font-extrabold text-slate-900 truncate']"));
-		Thread.sleep(500);
-		String actualProductText = productNameWishlistpage.getText();
-		Thread.sleep(500);
 		
-		AllVerifications.verifyTextPresent(driver, expectedProductText, productNameWishlistpage);
+//		WebElement productNameWishlistpage = driver.findElement(By.xpath("//h2[@class='text-[13px] font-extrabold text-slate-900 truncate']"));
+//		Thread.sleep(500);
+//		String actualProductText = productNameWishlistpage.getText();
+//		Thread.sleep(500);
+		
+//		if(expectedProductText.equals((actualProductText)))
+//		{
+//			System.out.println("Product successfully added to wishlist");
+//		}
+//		else
+//		{
+//			System.out.println("Product not added to wishlist");
+//		}
 		
 		// click on name drop down from header  
+		
 			WebElement nameDropDown = driver.findElement(By.xpath("//button[@class='inline-flex items-center gap-2 rounded-full border border-orange-200 bg-orange-50 px-4 py-2 text-[12px] font-extrabold text-orange-700 hover:bg-orange-500 hover:text-white hover:border-orange-500 transition uppercase']"));
-			AllVerifications.clickOnElementIfVisibleAndEnabled(driver,  nameDropDown);
+			Thread.sleep(500);
+			 nameDropDown.click();
+			Thread.sleep(500);
+			
 			
 			WebElement logoutButton = driver.findElement(By.xpath("//button[@class='block w-full text-left px-4 py-2.5 text-[13px] font-extrabold text-red-500 hover:bg-red-50']"));
-			AllVerifications.clickOnElementIfVisibleAndEnabled(driver,  logoutButton );
+			Thread.sleep(500);
+			 logoutButton.click();
+			Thread.sleep(500);
+			System.out.println(driver.getTitle());
 			
 		Thread.sleep(3000);
 		driver.quit();
 	}
+
 }
