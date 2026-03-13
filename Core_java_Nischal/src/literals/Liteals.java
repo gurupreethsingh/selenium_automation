ad42d427637e9dcf45d09d6c92183afec5f93b21
igurupreeth <igurupreeth@gmail.com>
2026-03-13 13:59:07 +0530

    updates

---
+++ Core_java_Nischal/src/literals/Liteals.java
@@ -0,0 +1,34 @@
+//  Literals : Numbers, characters, strings, boolean
+
+// Numbers - without any quotes , whole, decimal 
+// characters  - 'a' 'b' 
+// string  - ""
+// boolean - true - 1, false - 0  , without any quotes.
+
+package literals;
+
+public class Liteals {
+	// public - application level access(no permission is requied to run the code.)
+	// static - single copy.
+	// void - main functions return type
+	// main - it is the name of the funciton from where program should start.
+	// (JVM will look for main() function to start the program.
+	public static void main(String[] args) {
+		System.out.println(34);
+		System.out.println(3.4);
+		System.out.println(123 + 456);
+		System.out.println();
+		System.out.println('a');
+		System.out.println('g');
+		System.out.println('9');
+		System.out.println("Hello");
+		System.out.println("Hello welcome to ecoders");
+		System.out.println("Hello welcome             to ecoders");
+		System.out.println("hello 1234");
+		System.out.println("123" + "456");
+		System.out.println("Nischal" + " " + "Kumar");
+		System.out.println(true);
+		System.out.println(false);
+
+	}
+}
